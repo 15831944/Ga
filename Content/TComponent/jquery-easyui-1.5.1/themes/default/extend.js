47245ed6239aa79b130601f456acc43e595b7f10
qwe870691569 <35774991+qwe870691569@users.noreply.github.com>
2018-01-25 13:55:26 +0800

    zxl
    第一次提交

---
+++ Content/TComponent/jquery-easyui-1.5.1/themes/default/extend.js
@@ -0,0 +1,156 @@
+﻿(function ($) {
+
+    'use strict';
+
+    $(document).ready(function () { window.pm2 = new PM2UI(); });
+
+    //#region PM2UI
+
+    var PM2UI = function () {
+
+        var me = this;
+
+        me.body = $('body').layout();
+        me.header = me.body.layout('panel', 'north');
+        me.content = me.body.layout('panel', 'center');
+
+    };
+
+    PM2UI.prototype = {
+
+
+
+    };
+
+    //#endregion
+
+    //#region easyui-layout
+
+    $.extend($.fn.layout.defaults, {
+
+        fit: true
+
+    });
+
+    $.extend($.fn.layout.paneldefaults, {
+
+        border: false,
+        height: 'auto'
+
+    });
+
+    //#endregion
+
+    //#region easyui-panel
+
+    $.extend($.fn.panel.defaults, {
+
+
+
+    });
+
+    //#endregion
+
+    //#region easyui-tabs
+
+    $.extend($.fn.tabs.defaults, {
+
+        fit: true,
+        border: false,
+        tabPosition: 'bottom',
+        tabHeight: 30,
+        pill: true
+
+    });
+
+    //#endregion
+
+    //#region easyui-textbox
+
+    $.extend($.fn.textbox.defaults, {
+
+        height: 24,
+        width: 250,
+        labelAlign: 'right',
+        iconWidth: 28
+
+    });
+
+    //#endregion
+
+    //#region easyui-numberbox
+
+    $.extend($.fn.numberbox.defaults, {
+
+        height: 24,
+        width: 250,
+        labelAlign: 'right'
+
+    });
+
+    //#endregion
+
+    //#region easyui-numberbox
+
+    $.extend($.fn.numberspinner.defaults, {
+
+        height: 24,
+        width: 250,
+        labelAlign: 'right'
+
+    });
+
+    //#endregion
+
+    //#region easyui-combobox
+
+    $.extend($.fn.combobox.defaults, {
+
+        height: 24,
+        width: 150,
+        labelAlign: 'right',
+        iconWidth: 22
+
+    });
+
+    //#endregion
+
+    $.extend($.fn.combotree.defaults, {
+        height: 24,
+        width: 150,
+        labelAlign: 'right',
+        iconWidth: 28
+    });
+
+    //#region easyui-searchbox
+
+    $.extend($.fn.searchbox.defaults, {
+
+        height: 24,
+        width: 250,
+        labelAlign: 'right',
+        iconWidth: 22
+
+    });
+
+    //#endregion
+
+    //#region easyui-datebox
+
+    $.extend($.fn.datebox.defaults, {
+
+        height: 24,
+        width: 250,
+        labelAlign: 'right',
+        iconWidth: 22
+
+    });
+
+    //#endregion
+
+    $.extend($.fn.switchbutton.defaults, {
+        height: 24,
+        width: 48
+    });
+
+})(jQuery);
